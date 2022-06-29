509a4da3e4f2c5a0327a236f514d672c6b893b4a
Shantanu <amlesp@rknec.edu>
2022-06-29 16:10:28 +0530

    s

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+export const navBar = {
+    backgroundColor: "rgb(0, 0, 0)",
+    padding: "20px",
+    fontSize: "18px",
+};
+
+export const navBrand = {
+    fontSize: "27px",
+    marginLeft: "20px"
+};
+
+export const nav = {
+    marginLeft: "14px"
+};
